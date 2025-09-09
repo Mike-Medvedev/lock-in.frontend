c067de54a36afa82cfae866caa366343d5513fa4
Michael Medvedev <mmedvedev20@gmail.com>
2025-09-09 03:36:30 -0400

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "expo-router";
+import { Button, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter()
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+          title="Lock-In"
+          onPress={() => router.push("/lock-in")}
+        />
+    </View>
+  );
+}
